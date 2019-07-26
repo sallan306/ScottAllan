74f9cc4a18a618d68b0668dc036af9ff7164c9ef
sallan306 <sallan306@gmail.com>
2019-07-26 14:19:34 -0700

    changed the portfolio descriptions

--- src/Components/Site/MainWindow/Portfolio/Slideshow/PortfolioItems.js
+++ src/Components/Site/MainWindow/Portfolio/Slideshow/PortfolioItems.js
@@ -1,71 +1,72 @@
 const portfolioItems = [
 
         {
-        "id":"1",
-        "name": "BrewStash",
-        "tag":"Web Development",
-        "github": "https://github.com/sallan306/Brew-stash",
-        "liveLink": "https://brew-stash.herokuapp.com/",
-        "imgURL": require('./Images/BrewStash.png'),
-        "description": "A fun, lightweight website that helps brewers list their drink items and helps drinkers find them!",
-        "shortDescription" : "Make drinking even more fun",
-        "technologies": ["react","node.js","photoshop"]
-        
+                "id":"1",
+                "name": "BrewStash",
+                "tag":"Web Development",
+                "github": "https://github.com/sallan306/Brew-stash",
+                "liveLink": "https://brew-stash.herokuapp.com/",
+                "imgURL": require('./Images/BrewStash.png'),
+                "description": "A fun, lightweight website that helps brewers list their drink items and helps drinkers find them!",
+                "shortDescription" : "Make drinking even more fun",
+                "technologies": ["Handlebars","Node.js","Express"]
         },
         {
-        "id":"2",
-        "name": "I.Do",
-        "tag":"Web Development",
-        "github": "https://github.com/sallan306/I.Do",
-        "liveLink": "https://i-dooo.herokuapp.com/",
-        "imgURL": require('./Images/IDo.png'),
-        "description": "description here",
-        "shortDescription" : "Event planning just got easier",
-        "technologies": ["react","node.js","photoshop"]
+                "id":"2",
+                "name": "I.Do",
+                "tag":"Web Development",
+                "github": "https://github.com/sallan306/I.Do",
+                "liveLink": "https://i-dooo.herokuapp.com/",
+                "imgURL": require('./Images/IDo.png'),
+                "description": "description here",
+                "shortDescription" : "Event planning just got easier",
+                "technologies": ["React","Node.js","Mongo DB", "Nodemailer","Express"]
         },
         {
-        "id":"3",
-        "name": "Sunny Weekend",
-        "tag":"Web Development",
-        "github": "https://github.com/sallan306/SunnyWeekend",
-        "liveLink": "https://sallan306.github.io/SunnyWeekend/",
-        "imgURL": require('./Images/SunnyWeekend.png'),
-        "description": "description here",
-        "shortDescription" : "Step out of the shade!",
-        "technologies": ["react","node.js","photoshop"]
+                "id":"3",
+                "name": "Another Website",
+                "tag":"Web Development",
+                "github": "https://github.com/sallan306/ScottAllan",
+                "liveLink": "https://sallan306.github.io/ScottAllan/",
+                "imgURL": require('./Images/OldSite.png'),
+                "description": "My previous professional website. Cool effects and quite fun to make, yet I wanted to have a more accessable and less outlandish site. Using only the basics of web design. Space: the final frontier.",
+                "shortDescription" : "Space: The Final Frontier",
+                "technologies": ["HTML","CSS","Javascript"]
         },
         {
-        "id":"4",
-        "name": "Kindling",
-        "tag":"Web Development",
-        "github": "https://github.com/sallan306/FriendFinder",
-        "liveLink": "https://kindlingfriendfinder.herokuapp.com/",
-        "imgURL": require('./Images/kindling.png'),
-        "description": "description here",
-        "shortDescription" : "Find your superhero soulmate!",
-        "technologies": ["react","node.js","photoshop"]
+                "id":"4",
+                "name": "Sunny Weekend",
+                "tag":"Web Development",
+                "github": "https://github.com/sallan306/SunnyWeekend",
+                "liveLink": "https://sallan306.github.io/SunnyWeekend/",
+                "imgURL": require('./Images/SunnyWeekend.png'),
+                "description": "Follow the sun with this exciting website! Find your last minute sunny vacation weekend with our state of the art weather scrying technology! We don't know everything but were experts on the sun.",
+                "shortDescription" : "Step out of the shade!",
+                "technologies": ["javascript","API calls","CSS", "HTML"]
         },
         {
-        "id":"5",
-        "name": "Crystal Collector",
-        "tag":"Web Development",
-        "github": "https://github.com/sallan306/CrystalGame",
-        "liveLink": "https://sallan306.github.io/CrystalGame/",
-        "imgURL": require('./Images/CrystalCollectors.png'),
-        "description": "description here",
-        "shortDescription" : "Crystals everywhere!!",
-        "technologies": ["react","node.js","photoshop"]
+                "id":"5",
+                "name": "Kindling",
+                "tag":"Web Development",
+                "github": "https://github.com/sallan306/FriendFinder",
+                "liveLink": "https://kindlingfriendfinder.herokuapp.com/",
+                "imgURL": require('./Images/kindling.png'),
+                "description": "A fun little site where you answer a few questions to gague your personality type, then our expert level matchmaking system will pair you with your most compatible superhero sidekick!",
+                "shortDescription" : "Find your superhero soulmate!",
+                "technologies": ["react","node.js", "heroku","photoshop", "Express"]
         },
         {
-        "id":"6",
-        "name": "I.Do",
-        "tag":"Web Development",
-        "github": "https://github.com/sallan306/Brew-stash",
-        "liveLink": "https://brew-stash.herokuapp.com/",
-        "imgURL": require('./Images/BrewStash.png'),
-        "description": "description here",
-        "technologies": ["react","node.js","photoshop"]
-        }
+                "id":"6",
+                "name": "Crystal Collector",
+                "tag":"Web Development",
+                "github": "https://github.com/sallan306/CrystalGame",
+                "liveLink": "https://sallan306.github.io/CrystalGame/",
+                "imgURL": require('./Images/CrystalCollectors.png'),
+                "description": "Try your hand at collecting all the crystals! A simple yet addicting game where you win by acquiring the exact number of crystals...just make sure you dont get too much or its GAME OVER",
+                "shortDescription" : "Crystals everywhere!!",
+                "technologies": ["javascript","html"]
+        },
+
 ]
 
 export default portfolioItems
