6bcb09eefd87b59af913eb8cf9f8a5e838b5e88a
sallan306 <sallan306@gmail.com>
2019-08-03 00:13:11 -0700

    added some svgs to my services tab

--- src/Components/Site/MainWindow/Services/Services.js
+++ src/Components/Site/MainWindow/Services/Services.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react"; 
 import SEO from "./SVG/SEO"   
 import WebDesign from "./SVG/WebDesign"   
-import Responsive from "./SVG/Responsive"       
+import Responsive from "./SVG/Responsive"     
+import Border1 from "./SVG/Border1"  
+import Border2 from "./SVG/Border2"  
+import Border3 from "./SVG/Border3"  
 import "./Services.css"
 
 class Services extends Component {
@@ -31,19 +34,23 @@ componentDidMount() {
                     }}/>
                     <div className="servicesContainer">
                         <div className="serviceBox web">
+
                             <WebDesign 
                                 fillColor={this.props.fontColor}
                                 SVGSecondaryColor={this.props.SVGSecondaryColor}        />
+                            <Border1/>
                             <p style={{color: this.props.fontColor}} className="serviceBlurb">Fluid Front-End development with effective User Experience solutions that draws users back to your site</p>                       
                         </div>
                         <div className="serviceBox seo">
                             <SEO fillColor={this.props.fontColor}
                             SVGSecondaryColor={this.props.SVGSecondaryColor}/>
+                            <Border2/>
                             <p style={{color: this.props.fontColor}} className="serviceBlurb">Proper Search Engine Optimization and Indexing so your voice gets the attention that it deserves</p>  
                         </div>
                         <div className="serviceBox responsive">
                             <Responsive fillColor={this.props.fontColor}
                             SVGSecondaryColor={this.props.SVGSecondaryColor}    />
+                            <Border3/>
                             <p style={{color: this.props.fontColor}} className="serviceBlurb">Responsiveness across multiple platforms and devices. Your customers won't be limited by the shape of their screen</p>  
                         </div>
 
