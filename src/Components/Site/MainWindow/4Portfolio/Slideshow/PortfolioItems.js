71e2d1ffdc36a5f47ac834244c4373374a459e1a
sallan306 <sallan306@gmail.com>
2019-10-01 15:28:28 -0700

    changed portfolio items

--- src/Components/Site/MainWindow/4Portfolio/Slideshow/PortfolioItems.js
+++ src/Components/Site/MainWindow/4Portfolio/Slideshow/PortfolioItems.js
@@ -1,78 +1,77 @@
 const portfolioItems = [
+  {
+    id: "1",
+    name: "BrewStash",
+    tag: "Web Development",
+    github: "https://github.com/sallan306/Brew-stash",
+    liveLink: "https://brew-stash.herokuapp.com/",
+    imgURLWide: require("./Images/BrewStashWide.png"),
+    imgURLBox: require("./Images/BrewStashBox.png"),
+    description:
+      "A lightweight website that helps brewers list their drink items, prices, and brewery location. But most importantly, it helps drinkers drink them!",
+    shortDescription: "Make drinking even easier!",
+    technologies: ["Handlebars", "Node.js", "Express", "Ajax"]
+  },
+  {
+    id: "2",
+    name: "I.Do",
+    tag: "Web Development",
+    github: "https://github.com/sallan306/I.Do",
+    liveLink: "https://i-dooo.herokuapp.com/",
+    imgURLWide: require("./Images/IDoWide.png"),
+    imgURLBox: require("./Images/IDoBox.png"),
+    description:
+      "Ever throw a formal event, and hated trying to get everyone's addresses? Make them do it with this handy event planning app!",
+    shortDescription: "Event planning just got easier!",
+    technologies: [
+      "React",
+      "Node.js",
+      "Mongo DB",
+      "NodeEmailer",
+      "Express",
+      "Ajax",
+      "React-color"
+    ]
+  },
+  {
+    id: "3",
+    name: "Another Website",
+    tag: "Web Development",
+    github: "https://github.com/sallan306/ScottAllanSpaceHomepage/",
+    liveLink: "https://sallan306.github.io/ScottAllanSpaceHomepage/",
+    imgURLWide: require("./Images/OldSiteWide.png"),
+    imgURLBox: require("./Images/OldSiteBox.png"),
+    description:
+      "My previous professional website. Cool effects and quite fun to make, yet I wanted to have a more accessable and less outlandish site. Using only the basics of web design. Space: the final frontier.",
+    shortDescription: "Space: The Final Frontier",
+    technologies: ["HTML", "CSS", "Javascript"]
+  },
+  {
+    id: "5",
+    name: "Kindling",
+    tag: "Web Development",
+    github: "https://github.com/sallan306/FriendFinder",
+    liveLink: "https://kindlingfriendfinder.herokuapp.com/",
+    imgURLWide: require("./Images/KindlingWide.png"),
+    imgURLBox: require("./Images/KindlingBox.png"),
+    description:
+      "A fun little site where you answer a few questions to gague your personality type, then our expert level matchmaking system will pair you with your most compatible superhero sidekick!",
+    shortDescription: "Find your superhero soulmate!",
+    technologies: ["React", "Node.js", "Heroku", "Photoshop", "Express"]
+  },
+  {
+    id: "6",
+    name: "Crystal Collector",
+    tag: "Web Development",
+    github: "https://github.com/sallan306/CrystalGame",
+    liveLink: "https://sallan306.github.io/CrystalGame/",
+    imgURLWide: require("./Images/CrystalWide.png"),
+    imgURLBox: require("./Images/CrystalBox.png"),
+    description:
+      "Try your hand at collecting all the crystals! A simple yet addicting game where you win by acquiring the exact number of crystals...just make sure you dont get too much or its GAME OVER",
+    shortDescription: "Crystals everywhere!!",
+    technologies: ["javascript", "Html", "CSS"]
+  }
+];
 
-        {
-                "id":"1",
-                "name": "BrewStash",
-                "tag":"Web Development",
-                "github": "https://github.com/sallan306/Brew-stash",
-                "liveLink": "https://brew-stash.herokuapp.com/",
-                "imgURLWide": require('./Images/BrewStashWide.png'),
-                "imgURLBox" : require('./Images/BrewStashBox.png'),
-                "description": "A lightweight website that helps brewers list their drink items, prices, and brewery location. But most importantly, it helps drinkers drink them!",
-                "shortDescription" : "Make drinking even easier!",
-                "technologies": ["Handlebars","Node.js","Express", "Ajax"]
-        },
-        {
-                "id":"2",
-                "name": "I.Do",
-                "tag":"Web Development",
-                "github": "https://github.com/sallan306/I.Do",
-                "liveLink": "https://i-dooo.herokuapp.com/",
-                "imgURLWide": require('./Images/IDoWide.png'),
-                "imgURLBox": require('./Images/IDoBox.png'),
-                "description": "Ever throw a formal event, and hated trying to get everyone's addresses? Make them do it with this handy event planning app!",
-                "shortDescription" : "Event planning just got easier!",
-                "technologies": ["React","Node.js","Mongo DB", "NodeEmailer","Express", "Ajax","React-color"]
-        },
-        {
-                "id":"3",
-                "name": "Another Website",
-                "tag":"Web Development",
-                "github": "https://github.com/sallan306/ScottAllanSpaceHomepage/",
-                "liveLink": "https://sallan306.github.io/ScottAllanSpaceHomepage/",
-                "imgURLWide": require('./Images/OldSiteWide.png'),
-                "imgURLBox": require('./Images/OldSiteBox.png'),
-                "description": "My previous professional website. Cool effects and quite fun to make, yet I wanted to have a more accessable and less outlandish site. Using only the basics of web design. Space: the final frontier.",
-                "shortDescription" : "Space: The Final Frontier",
-                "technologies": ["HTML","CSS","Javascript"]
-        },
-        {
-                "id":"4",
-                "name": "Sunny Weekend",
-                "tag":"Web Development",
-                "github": "https://github.com/sallan306/SunnyWeekend",
-                "liveLink": "https://sallan306.github.io/SunnyWeekend/",
-                "imgURLWide": require('./Images/SunnyWeekendWide.png'),
-                "imgURLBox": require('./Images/SunnyWeekendBox.png'),
-                "description": "Follow the sun with this exciting website! Find your perfect spontaneous vacation weekend with our state of the art weather scrying technology! We don't know everything but were experts on the sun.",
-                "shortDescription" : "Step out of the shade and plan a weekend getaway!",
-                "technologies": ["Javascript","API calls","CSS", "HTML"]
-        },
-        {
-                "id":"5",
-                "name": "Kindling",
-                "tag":"Web Development",
-                "github": "https://github.com/sallan306/FriendFinder",
-                "liveLink": "https://kindlingfriendfinder.herokuapp.com/",
-                "imgURLWide": require('./Images/KindlingWide.png'),
-                "imgURLBox": require('./Images/KindlingBox.png'),
-                "description": "A fun little site where you answer a few questions to gague your personality type, then our expert level matchmaking system will pair you with your most compatible superhero sidekick!",
-                "shortDescription" : "Find your superhero soulmate!",
-                "technologies": ["React","Node.js", "Heroku","Photoshop", "Express"]
-        },
-        {
-                "id":"6",
-                "name": "Crystal Collector",
-                "tag":"Web Development",
-                "github": "https://github.com/sallan306/CrystalGame",
-                "liveLink": "https://sallan306.github.io/CrystalGame/",
-                "imgURLWide": require('./Images/CrystalWide.png'),
-                "imgURLBox": require('./Images/CrystalBox.png'),
-                "description": "Try your hand at collecting all the crystals! A simple yet addicting game where you win by acquiring the exact number of crystals...just make sure you dont get too much or its GAME OVER",
-                "shortDescription" : "Crystals everywhere!!",
-                "technologies": ["javascript","Html", "CSS"]
-        },
-
-]
-
-export default portfolioItems
+export default portfolioItems;
